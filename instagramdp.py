f80ff43a5ff3120b5fd8f5cfd31b8ecf9b6c02c4
vinithreddyb <116493221+vinithreddyb@users.noreply.github.com>
2022-10-27 19:33:02 +0530

    Update instagramdp.py

--- instagramdp.py
+++ instagramdp.py
@@ -1,7 +1,6 @@
 
 import instaloader
- 
 
 ig = instaloader.Instaloader()
 dp = input("Enter Insta username : ")
-ig.download_profile(dp , profile_pic_only=True
+ig.download_profile(dp , profile_pic_only=True)
